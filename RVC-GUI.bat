a1eae179af066accd6f0bfd11784ad625f8baeee
Nasser Aldosari <nasser11r1@gmail.com>
2023-05-25 01:18:42 +0300

    v2 fix

--- RVC-GUI.bat
+++ RVC-GUI.bat
@@ -1,13 +1,22 @@
 @echo off
 
-rem Check if the specific Python runtime folder exists
-if exist "runtime\python.exe" (
-    echo Running using the specific Python runtime.
-    runtime/python.exe rvcgui.py --pycmd runtime/python.exe
-pause
+REM Get the path of the script's directory
+set "scriptDir=%~dp0"
 
+REM Set the path to the Python runtime folder
+set "runtimeFolder=%scriptDir%runtime"
+
+REM Check if the runtime folder exists
+
+REM Check if the runtime folder exists
+if exist "%runtimeFolder%\python.exe" (
+    REM Runtime folder exists, so run the file using the runtime Python
+    echo Running with the runtime Python.
+     "runtime/python.exe" rvcgui.py --pycmd "runtime/python.exe"
+      pause
 ) else (
-    echo Running using the system Python.
-     python.exe rvcgui.py --pycmd python.exe
+    REM Runtime folder does not exist, so run the file using the system Python
+     echo Running with the system Python.
+    python.exe rvcgui.py --pycmd python.exe
 pause
 )
